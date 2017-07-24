ea96558b4cbdbf55c50d4e5a8c15a54234689afd
xxx <luke.marty@gmail.com>
2017-07-23 22:52:27 -0500

    testing

--- Defcon25Game/Defcon25Game.ino
+++ Defcon25Game/Defcon25Game.ino
@@ -6,6 +6,15 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 #include "Adafruit_NeoPixel.h"
+#include "Mac.h"
+#include "ESP8266WiFi.h"
+//#include <IoTtweetESP32.h>
+#include <ArduinoJson.h>
+//#include "user_interface.h"
+//#include "ip_addr.h"
+//#include <Adafruit_ESP8266.h>
+
+
 
 //DEVICES
 // TFT Values
@@ -40,6 +49,34 @@ const int e7 = 2637;
 const int f7 = 2794;
 const int g7 = 3136;
 
+// for deauth
+extern "C" {
+  #include "user_interface.h"
+}
+
+
+//===== Deauth Detect SETTINGS =====//
+#define channel 1 //the channel it should scan on (1-14)
+#define channelHopping true //scan on all channels
+#define maxChannel 13 //US = 11, EU = 13, Japan = 14
+#define ledPin 2 //led pin ( 2 = built-in LED)
+#define inverted true // invert HIGH/LOW for the LED
+#define packetRate 3 //min. packets before it gets recognized as an attack
+//=================================//
+
+
+//================deauth vars===========//
+//Mac from;
+//Mac to;
+unsigned long c = 0;
+unsigned long prevTime = 0;
+unsigned long curTime = 0;
+int curChannel = channel;
+//=======================================//
+
+#define scanTime 500 //scan time per channel in ms
+
+
 // init the display and leds
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXELPIN, NEO_GRB + NEO_KHZ800);
@@ -51,6 +88,7 @@ int mute_touched = false;
 char ssid[]="HackerBoxer_MrmuthafuckinE";  //put your handle after the underscore
 int muted = true; // default to muted
 unsigned long debounce;
+char infectionState = "clean"; // clean, infected, carrier
 
 
 
@@ -71,6 +109,18 @@ void setup()
     return;
   }
   Serial.println("initialization done.");
+
+  //===============deauth =============//
+  wifi_set_opmode(STATION_MODE);
+wifi_promiscuous_enable(0);
+WiFi.disconnect();
+wifi_set_promiscuous_rx_cb(sniffer);
+wifi_set_channel(curChannel);
+wifi_promiscuous_enable(1);
+
+//   pinMode(ledPin, OUTPUT); // find another notifier!!
+
+//===================================//
 
   /* load the root object with the contents of the filesystem "/" */
   root = SD.open("/");
@@ -156,11 +206,75 @@ void loop()
 
   // stop broadcating SSID (AP off)
   WiFi.softAPdisconnect(1);
+
+  // ======== start deauth detect loop==========//
+
+  curTime = millis();
+
+  if(curTime - prevTime >= scanTime){
+    prevTime = curTime;
+    Serial.println((String)c);
+    // FIND A BETTER NOTIFICATION
+    if(c >= packetRate){
+      tft.setCursor(0, 0);
+      tft.setTextColor(ILI9341_YELLOW);
+      tft.setTextSize(4);
+      tft.println(" SOMEONE IS RUNNING A DEAUTH ATTACK");
+      tft.setTextColor(ILI9341_RED);
+      tft.setTextSize(2);
+    }else{
+      tft.setCursor(0, 0);
+      tft.setTextColor(ILI9341_YELLOW);
+      tft.setTextSize(4);
+      tft.println(" Not seeing any deauths right now");
+      tft.setTextColor(ILI9341_RED);
+      tft.setTextSize(2);
+    }
+
+    c = 0;
+    if(channelHopping){
+      curChannel++;
+      if(curChannel > maxChannel) curChannel = 1;
+      wifi_set_channel(curChannel);
+    }
+  }
+  //================= end deauth ================//
 }
 
 //
 // OUR FUNCTIONS
 //
+
+//
+// Need implementation
+//
+
+void otaserver() {
+  // webserver component required for OTA updates.  Will hold binaries for downloading, and that's it
+  // pin to a core and run constantly if possible.
+};
+
+void getupdate() {
+  // when connected to a hackerboxes AP, check to see if there is an update available
+};
+
+void checkInfected(){
+  //when connected to a hackerboxes AP, check if remote end is infected.
+};
+
+void infectedFreakout(){
+  // go batshit if in contact with an infected badge
+};
+
+void transferInfection(){
+  // transfer our virus(s) and change infected state
+};
+
+void showInfection(){
+  //show infection state on the main badge loop
+};
+
+
 
 void mute_handler() // if the mut button has been touched, flip variable
 {
@@ -239,6 +353,27 @@ void wifiScan2LCD()
     }
   }
 }
+
+//===================deauth funcs =============//
+
+void sniffer(uint8_t *buf, uint16_t len) {
+  //if(len>27){
+    //from.set(buf[16],buf[17],buf[18],buf[19],buf[20],buf[21]);
+    //to.set(buf[22],buf[23],buf[24],buf[25],buf[26],buf[27]);
+
+    if(buf[12] == 0xA0 || buf[12] == 0xC0){
+      /*Serial.print("From ");
+      from._println();
+      Serial.print("To ");
+      to._println();
+      Serial.println();*/
+
+      c++;
+    }
+
+  //}
+}
+
 
 // Our funtion to loop throught the filesystem and print index to serial
 void printDirectory(File dir, int numTabs) {
